7c0c5eea6ad105d579b36baac43d007053270043
Smiley <smileyismail67@gmail.com>
2022-11-20 06:16:36 +0530

    made responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 
-// import logo from "../assets/logo.png";
+import logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
@@ -13,22 +13,49 @@ const Footer = () => {
           PICK A PLAN
         </button>
       </div>
-      {/* <div className="grid-cols-5">
-        <div className="flex justify-center items-center gap-4">
-          <img src={logo} alt="NinjaMail" className="lg:w-24" />
-          <h1 className="font-extrabold text-3xl lg:text-4xl">NinjaMail</h1>
+
+      <div className="flex flex-col sm:flex-row justify-start items-start gap-10  mt-20">
+        <div className="flex justify-start items-start gap-4">
+          <img src={logo} alt="NinjaMail" className="lg:w-20" />
+          <h1 className="font-extrabold text-xl lg:text-2xl">NinjaMail</h1>
         </div>
 
-        <div>
-          <a href="#">Features</a>
-          <a href="#">Pricing</a>
-          <a href="#">Services</a>
-          <a href="#">Partners</a>
+        <div className="grid grid-cols-2 sm:grid-cols-4 font-semibold gap-10">
+          <div className="flex flex-col justify-start  items-start">
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Services</a>
+            <a href="#">Partners</a>
+          </div>
+
+          <div className="flex flex-col justify-start items-start">
+            <a href="#">About Us</a>
+            <a href="#">Tutorials</a>
+            <a href="#">Resources</a>
+            <a href="#">Help Center</a>
+            <a href="#">Templates</a>
+            <a href="#">Case Studies</a>
+          </div>
+
+          <div className="flex flex-col justify-start items-start">
+            <a href="#">Medium</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+            <a href="#">Linkedin</a>
+            <a href="#">FaceBook</a>
+          </div>
+
+          <div className="flex flex-col justify-start items-start">
+            <a href="#">Contact Us</a>
+            <a href="#">Slack</a>
+            <a href="#">Job</a>
+          </div>
         </div>
-      </div> */}
+      </div>
+
       <div className="border-2 w-4/5 sm:w-2/3 mt-10 mb-10"></div>
       <div className="w-screen text-sm font-semibold flex justify-start sm:justify-center  items-start flex-wrap gap-10 mb-10 px-10">
-        <p className="w-2/3">
+        <p className="w-10/12 sm:w-2/3">
           NinjaMail is a sample project for Quest AI. © 2019 Quest AI, All
           rights reserved.
         </p>
