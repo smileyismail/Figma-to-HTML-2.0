bfd6a8f370e54802091d4cb109b3e6ee0d18158b
Smiley <smileyismail67@gmail.com>
2022-11-28 20:21:03 +0530

    Small Mistakes corrected

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -15,12 +15,16 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col sm:flex-row justify-start items-start gap-10  mt-20">
-        <div className="flex justify-start items-start gap-4">
-          <img src={logo} alt="NinjaMail" className="lg:w-20" />
+        <div className="flex justify-start items-start gap-0">
+          <img
+            src={logo}
+            alt="NinjaMail"
+            className="lg:w-20 -translate-x-4 translate-y-1"
+          />
           <h1 className="font-extrabold text-2xl lg:text-3xl">NinjaMail</h1>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-4 font-semibold gap-10">
+        <div className="hidden sm:grid grid-cols-2 sm:grid-cols-4 font-semibold gap-10">
           <div className="flex flex-col justify-start  items-start">
             <a href="#">Features</a>
             <a href="#">Pricing</a>
@@ -46,6 +50,33 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col justify-start items-start">
+            <a href="#">Contact Us</a>
+            <a href="#">Slack</a>
+            <a href="#">Job</a>
+          </div>
+        </div>
+
+        {/* for mobile devices */}
+        <div className="grid sm:hidden grid-cols-2 sm:grid-cols-4 font-semibold gap-10">
+          <div className="flex flex-col justify-start  items-start">
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Services</a>
+            <a href="#">Partners</a>
+            <a href="#">About Us</a>
+            <a href="#">Tutorials</a>
+            <a href="#">Resources</a>
+            <a href="#">Help Center</a>
+            <a href="#">Templates</a>
+            <a href="#">Case Studies</a>
+          </div>
+
+          <div className="flex flex-col justify-start items-start">
+            <a href="#">Medium</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+            <a href="#">Linkedin</a>
+            <a href="#">FaceBook</a>
             <a href="#">Contact Us</a>
             <a href="#">Slack</a>
             <a href="#">Job</a>
